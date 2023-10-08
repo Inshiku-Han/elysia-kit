64d0fdc0108e9b4f465e00ace9bc18580e29d465
insik-han <gksdudrms0@icloud.com>
2023-10-08 21:20:07 +0900

    set prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
-import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { PrismaClient } from "@prisma/client";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new PrismaClient();
+
+const users = new Elysia({ prefix: "/users" })
+  .get("/", async () => db.user.findMany(), {
+    detail: { tags: ["Users"] },
+  })
+  .post(
+    "/",
+    async ({ body }) =>
+      db.user.create({
+        data: body,
+        select: { id: true, name: true, email: true },
+      }),
+    {
+      error: ({ error }) => {
+        console.log(error);
+        return error;
+      },
+      body: t.Object({
+        email: t.String(),
+        name: t.Optional(t.String()),
+      }),
+      detail: { tags: ["Users"] },
+    }
+  );
+
+const app = new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        tags: [{ name: "Users", description: "Users related endpoints" }],
+      },
+    })
+  )
+  .group("/v1", (app) => app.get("/", () => "Using v1").use(users))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
