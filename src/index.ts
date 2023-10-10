cfd76c3b70cc7bb48df7c885c8a008d05afd77d0
insik-han <gksdudrms0@icloud.com>
2023-10-10 15:41:22 +0900

    fix: swagger to v1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
-import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import v1 from "./v1";
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
-  .use(
-    swagger({
-      path: "/swagger",
-      documentation: {
-        info: {
-          title: "Elysia",
-          version: "0.1.0",
-        },
-        tags: [{ name: "Users", description: "Users related endpoints" }],
-      },
-    })
-  )
   .use(v1)
   .listen(4000);
 
--- src/v1/index.ts
+++ src/v1/index.ts
@@ -1,8 +1,20 @@
 import Elysia from "elysia";
 import users from "./user";
+import { swagger } from "@elysiajs/swagger";
 
-const v1 = new Elysia().group("/v1", (app) =>
-  app.get("/", () => "Using v1").use(users)
-);
+const v1 = new Elysia({ name: "V1", prefix: "/v1" })
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Elysia REST V1",
+          version: "0.1.0",
+        },
+        tags: [{ name: "Users", description: "Users related endpoints" }],
+      },
+    })
+  )
+  .get("/", () => "Using v1")
+  .use(users);
 
 export default v1;
