4f080f9667e0222b3a4b63de5199d846216086fa
insik-han <gksdudrms0@icloud.com>
2023-11-15 16:39:35 +0900

    add editorconfig, update dependency

--- src/index.ts
+++ src/index.ts
@@ -9,55 +9,55 @@ const userTag: Tag = { name: 'Users', description: 'Users related endpoints' }
 const tags: Tag[] = [userTag]
 
 const app = new Elysia()
-  .get('/', () => 'Hello Elysia')
-  .use(
-    swagger({
-      path: '/swagger',
-      documentation: {
-        info: {
-          title: 'Elysia',
-          description: 'API description',
-          version: '0.1.0',
-        },
-        tags,
-      },
-    }),
-  )
-  .use(
-    new Elysia({ name: userTag.name, prefix: '/users' })
-      .model({
-        usersResponse: t.Array(
-          t.Object({
-            id: t.Number(),
-            email: t.String(),
-            name: t.Unsafe({ ...t.String(), nullable: true }),
-          }),
-        ),
-        usersCreateBody: t.Object({
-          email: t.String(),
-          name: t.Optional(t.String()),
-        }),
-      })
-      .get('/', () => db.select().from(users), {
-        response: 'usersResponse',
-        detail: {
-          tags: [userTag.name],
-        },
-      })
-      .post('/', ({ body }) => db.insert(users).values(body).returning(), {
-        body: 'usersCreateBody',
-        response: 'usersResponse',
-        error: ({ error }) => {
-          console.log(error)
-          return error
-        },
-        detail: {
-          tags: [userTag.name],
-        },
-      }),
-  )
-  .listen(4000)
+	.get('/', () => Bun.file('src/assets/elysia.svg'))
+	.use(
+		swagger({
+			path: '/swagger',
+			documentation: {
+				info: {
+					title: 'Elysia',
+					description: 'API description',
+					version: '0.1.0',
+				},
+				tags,
+			},
+		}),
+	)
+	.use(
+		new Elysia({ name: userTag.name, prefix: '/users' })
+			.model({
+				usersResponse: t.Array(
+					t.Object({
+						id: t.Number(),
+						email: t.String(),
+						name: t.Unsafe({ ...t.String(), nullable: true }),
+					}),
+				),
+				usersCreateBody: t.Object({
+					email: t.String(),
+					name: t.Optional(t.String()),
+				}),
+			})
+			.get('/', () => db.select().from(users), {
+				response: 'usersResponse',
+				detail: {
+					tags: [userTag.name],
+				},
+			})
+			.post('/', ({ body }) => db.insert(users).values(body).returning(), {
+				body: 'usersCreateBody',
+				response: 'usersResponse',
+				error: ({ error }) => {
+					console.log(error)
+					return error
+				},
+				detail: {
+					tags: [userTag.name],
+				},
+			}),
+	)
+	.listen(4000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 )
